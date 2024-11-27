b8ea80e638650056525e39850091c5e5a31a42f0
reedstec <160498243+reedstec@users.noreply.github.com>
2024-11-27 12:44:21 +0000

    Update DattoInstall.ps1

--- DattoInstall.ps1
+++ DattoInstall.ps1
@@ -1,1 +1,8 @@
 #PlaceHolder for the localised installer
+# Check if the service "Cagservice" exists
+$service = Get-Service -Name "Cagservice" -ErrorAction SilentlyContinue
+
+# If the service does not exist, run the executable
+if (-not $service) {
+    & "C:\DattoAgent\downloadAgent.exe"
+}
